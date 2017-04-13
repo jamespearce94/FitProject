56468b63bb78c17ef5731470b2875e50af726b7f
James Pearce <jpearcey1000@gmail.com>
2017-04-13 23:35:20 +0000

    - implement challenge leaders

--- src/modals/view-challenge/view-challenge.modal.ts
+++ src/modals/view-challenge/view-challenge.modal.ts
@@ -11,6 +11,7 @@ export class ViewChallengeModal implements OnInit {
 
     challenge = null;
     loading = true;
+    leader: any = null;
 
     constructor(private viewCtrl: ViewController,
                 private _userService : UserService) {
@@ -22,18 +23,44 @@ export class ViewChallengeModal implements OnInit {
         this._userService.getUserList()
             .subscribe( users => {
                 this.challenge.participants.forEach( participant => {
-                    if( participant.progress )
-                    {
-                        participant.percentage = Math.round((participant.progress / this.challenge.completion.required)*100);
-                    }else {
+                    if(participant.progress < 1){
                         participant.percentage = 0;
                     }
-
-                    participant.percentage = participant.percentage <= 100 ? participant.percentage : 100;
-
-
+                    else{
+                        participant.percentage = Math.round((participant.progress / this.challenge.completion.required)*100);
+                    }
                     let user = users.find( user => user.$key === participant.id );
                     Object.assign(participant, user);
+                    this.challenge.participants.forEach((participant)=>{
+                        if (this.leader == null){
+                            this.leader = participant;
+                        }
+                        else{
+                            if(participant.complete_date && this.leader.complete_date != null)
+                            {
+                                if(participant.complete_date < this.leader.complete_date){
+                                    this.leader = participant;
+                                }
+                                if(participant.progress > this.leader.progress)
+                                {
+                                    this.leader = participant;
+                                }
+                            }
+                            else {
+                                if(participant.progress > this.leader.progress)
+                                {
+                                    this.leader = participant;
+                                }
+                            }
+
+
+                        }
+                    });
+                    if(this.leader != null){
+                        this.challenge.participants = this.challenge.participants.filter((participant) => {
+                            return participant.id != this.leader.id
+                        })
+                    }
                 });
                 this.loading = false;
             });
